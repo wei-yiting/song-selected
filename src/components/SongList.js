44a830416d17c60b4b7ef584d8c2741bee5f421f
WYT <tina.yiting.wei@gmail.com>
2021-06-16 22:47:30 +0800

    build react-redux connect to SongList component

---
+++ src/components/SongList.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const SongList = ({ songs }) => {
+  const renderedList = () => {
+    return songs.map((song) => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button className="ui button primary">Select</button>
+          </div>
+          <div className="content">{song.title}</div>
+        </div>
+      );
+    });
+  };
+
+  return <div className="ui divided list">{renderedList()}</div>;
+};
+
+const mapStateToProps = (state) => {
+  return { songs: state.songs };
+};
+
+export default connect(mapStateToProps)(SongList);
